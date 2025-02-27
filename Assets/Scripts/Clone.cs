088b698dcddb21e7d3318be0f8c102fa0d3893e9
Emile Birraux <112955984+Birox1er@users.noreply.github.com>
2025-02-27 11:04:15 +0100

    [MOD] Spawn clone change
    changé la façon dont le clone est spawn pour fit avec le nouveau concept reste à le lier avec les niveaux

--- Assets/Scripts/Clone.cs
+++ Assets/Scripts/Clone.cs
@@ -8,22 +8,22 @@ public class Clone : MonoBehaviour
     [SerializeField] private List<GameObject> _clones;
     [SerializeField] private int _currentClone = 0;
     [SerializeField] private int _charID;
+    [SerializeField] private GameObject _spawnPoint;//temp
+
 
     private void Start()
     {
         _charID = CloneManager.instance.Characters.Count;
         CloneManager.instance.Characters.Add(this);
     }
-    public void Cloned(InputAction.CallbackContext context)
+    public void Cloned(GameObject spawnPoint)
     {
-        if (context.performed)
-        {
-            if (_currentClone < _clones.Count)
+            if (_currentClone < _clones.Count)//condition à changer.
             {
-                GameObject instantiatedClone = Instantiate(_clones[_currentClone], transform.position, transform.rotation);
+                GameObject instantiatedClone = Instantiate(_clones[_currentClone], _spawnPoint.transform.position, _spawnPoint.transform.rotation);
                 _currentClone++;
             }
-        }
+        
     }
     public void Switch(InputAction.CallbackContext context)
     {
