ae4184c07c44d65054af0466e751bdd1318cfb9a
Selsanat <leandro.benites@edu.devinci.fr>
2025-03-05 11:55:19 +0100

    [ ADD ] : Started to integrate State Switching

--- Assets/Scripts/PlayerStateMachine/PlayerStateMachine.cs
+++ Assets/Scripts/PlayerStateMachine/PlayerStateMachine.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerStateMachine : MonoBehaviour
 {
+    private Vector2 _velocity;
+    public Vector2 Velocity
+    {
+        get { return _velocity; }
+        set { _velocity = value; }
+    }
+
+    //public TemplateState StartState => stateIdle;
+    //public TemplateState CurrentState { get; private set; }
+    //public TemplateState PreviousState { get; private set; }
 
     public void ChangeState(PlayerState state)
     {
