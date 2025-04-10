3519e17dc40dfc185135972eba2e2edd8b256d8e
Emile Birraux <112955984+Birox1er@users.noreply.github.com>
2025-04-10 11:06:33 +0200

    [ADD] Début gestionnaire de scène

---
+++ Assets/Scripts/Managers/ScenesManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public static ScenesManager  instance = null;
+    [SerializeField]private string _nextScene;
+    [SerializeField]private string _menuScene;
+    [SerializeField]private GameObject _Scene;
+    [SerializeField]private bool _isMainLevel;
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        SceneManager.LoadSceneAsync(_nextScene);
+        StartCoroutine(Deload());
+    }
+    public void loadNextScene()
+    {
+        SceneManager.LoadSceneAsync(_nextScene);
+        StartCoroutine(Deload());
+    }
+    IEnumerator Deload()
+    {
+        if (_isMainLevel)
+        {
+            Destroy(_Scene);
+            //active l'anim de deload
+        }
+        yield return new WaitForSeconds(1);//durée de l'anim
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        yield return null;
+    }
+}
