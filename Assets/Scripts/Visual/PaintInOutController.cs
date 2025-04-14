a0062c32ee5802d0f6e4eb35e82db9628d77607f
Emile Birraux <112955984+Birox1er@users.noreply.github.com>
2025-04-14 14:27:05 +0200

    [MOD] On avance sur le truc de spawn de niveaux

--- Assets/Scripts/Visual/PaintInOutController.cs
+++ Assets/Scripts/Visual/PaintInOutController.cs
@@ -7,20 +7,27 @@ public class PaintInOutController : MonoBehaviour
 {
     private Material _material;
     [SerializeField]float _duration=0.2f;
+    [SerializeField]GameObject _firstPaint;
+    RectTransform _rectTransform;
+    RawImage _image;
 
     private void Awake()
     {
         _material = GetComponent<RawImage>().material;
+        _rectTransform = GetComponent<RectTransform>();
+        _image = GetComponent<RawImage>();
     }
     private void Start()
     {
-        PaintIn();
+        PaintIn(_firstPaint);
     }
-    private void PaintIn()
+    private void PaintIn(GameObject paint)// objet , position taille
     {
-        StartCoroutine(ShaderIn());
+        _rectTransform = paint.GetComponent<RectTransform>();
+        _image.enabled = true;
+        StartCoroutine(ShaderIn(paint));
     }
-    IEnumerator ShaderIn()
+    IEnumerator ShaderIn(GameObject paint)
     {
         float timer = 0;
         while (timer < _duration)
@@ -29,6 +36,12 @@ public class PaintInOutController : MonoBehaviour
             timer += Time.deltaTime;
             yield return null;
         }
+        paint.layer = 0;
+        foreach (Transform child in paint.transform)
+        {
+            child.gameObject.layer = 0;
+        }
+        _image.enabled = false;
         yield return null;
     }
 }
