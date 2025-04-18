5148c9bc8531473948947ee7ae1614309f5663a7
Romain Mousty <fun.gaming72200@gmail.com>
2025-04-18 09:39:44 +0200

    [MOD] Remove CameraManager decrease time / distance

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -220,7 +220,7 @@ public class CameraManager : MonoBehaviour
 
         float decrease = targetOrthoSize - startOrthoSize;
 
-        _cameraDezoomTime = Mathf.Abs(decrease) / _cameraDezoomTime;
+        //_cameraDezoomTime = Mathf.Abs(decrease) / _cameraDezoomTime;
 
         while (elapsedTime < _cameraDezoomTime)
         {
@@ -287,7 +287,7 @@ public class CameraManager : MonoBehaviour
         float targetOrthoSize = initOrthoSize;
 
         float increase = targetOrthoSize - startOrthoSize;
-        _cameraZoomTime = Mathf.Abs(increase) / _cameraZoomTime;
+        //_cameraZoomTime = Mathf.Abs(increase) / _cameraZoomTime;
 
         Destroy(_globalCamera.gameObject);
         _globalCamera = null;
