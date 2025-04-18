721f7930d4c035ad38708304fed2eeaf25a73dda
Romain Mousty <fun.gaming72200@gmail.com>
2025-04-18 09:48:59 +0200

    [MOD] CameraManager

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -247,7 +247,7 @@ public class CameraManager : MonoBehaviour
     IEnumerator ShowAndHideLevel()
     {
         SeeAllLevels();
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(_cameraDezoomTime);
         FocusCamera();
     }
 
