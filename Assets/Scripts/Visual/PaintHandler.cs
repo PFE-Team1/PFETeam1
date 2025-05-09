275da66a5b3ee4325e23631121eca1f64658d6cf
Selsanat <leandro.benites@edu.devinci.fr>
2025-05-09 12:43:06 +0200

    [ ADD ] : Event handling for Animation Key. Needs integration

--- Assets/Scripts/Visual/PaintHandler.cs
+++ Assets/Scripts/Visual/PaintHandler.cs
@@ -1,3 +1,4 @@
+using Spine;
 using Spine.Unity;
 using UnityEngine;
 
@@ -5,5 +6,27 @@ public class PaintHandler : MonoBehaviour
 {
     [SerializeField] public SkeletonPartsRenderer BodyPartRenderer;
     [SerializeField] public SkeletonPartsRenderer ArmPartRenderer;
+    [HideInInspector] public SkeletonMecanim SkeletonMecanim;
 
+    private Spine.AnimationState spineAnimationState;
+
+    // to capture event "Footstep" when it's placed outside of folders
+    void Grab()
+    {
+    }
+    void Clonecreation()
+    {
+    }
+
+    void Dooropening()
+    {
+    }
+
+    void Place()
+    {
+    }
+
+    void Pushbutton()
+    {
+    }
 }
