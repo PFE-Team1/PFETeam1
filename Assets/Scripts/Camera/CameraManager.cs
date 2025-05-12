bf800189c1eb23f0cd2201d578105eae5add026e
Emile Birraux <112955984+Birox1er@users.noreply.github.com>
2025-05-12 21:35:09 +0200

    [FIX] 720 719

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -28,6 +28,7 @@ public class CameraManager : MonoBehaviour
     public CinemachineVirtualCamera MainCamera { get => _mainCamera; set => _mainCamera = value; }
     public GameObject CompositeParent { get => _compositeParent; set => _compositeParent = value; }
     public List<GameObject> Levels { get => _levels; set => _levels = value; }
+    public float CameraDezoomTime { get => _cameraDezoomTime; }
 
     void Awake()
     {
