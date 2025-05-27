cee9243ffdf0a8b29e456912532bb8e456adae27
Emile Birraux <112955984+Birox1er@users.noreply.github.com>
2025-05-27 10:22:24 +0200

    [MOD] managers en gros

--- Assets/Scripts/Visual/DoorVFX.cs
+++ Assets/Scripts/Visual/DoorVFX.cs
@@ -45,9 +45,12 @@ public class DoorVFX : MonoBehaviour
     public void StopDoorVFX()
     {
         // _otherDoorVFX.DoorVFXInstance = null;
-        foreach (DoorMaterialInstance child in _doorVFXInstance.transform.GetChild(0).GetComponentsInChildren<DoorMaterialInstance>())
+        if (_doorVFXInstance)
         {
-            child.Out(this);
+            foreach (DoorMaterialInstance child in _doorVFXInstance.transform.GetChild(0).GetComponentsInChildren<DoorMaterialInstance>())
+            {
+                child.Out(this);
+            }
         }
         _doorVFXInstance = null;
         //_otherDoorVFX.OtherDoorVFX = null;
