75eb61fcbb5dbd88ecdcaa04cc420956c62e03cc
Selsanat <leandro.benites@edu.devinci.fr>
2025-06-04 11:36:10 +0200

    [ ADD ] : Letterbox Effect & UIManager

---
+++ Assets/Scripts/Sequencer/DerivedStates/UI/Scripts/showLetterBoxGameAction.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showLetterBoxGameAction : AGameAction
+{
+
+    [Header("Action Settings")]
+    [SerializeField] private string debugMessage = "Debug Game Action Executed";
+    protected override void OnExecute()
+    {
+    }
+    protected override void OnUpdate(float deltaTime)
+    {
+    }
+    protected override void OnEnd()
+    {
+    }
+}
