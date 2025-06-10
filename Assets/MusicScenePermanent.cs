64a7a2982499771a65d6c89785b4fb425e048702
Romain Mousty <fun.gaming72200@gmail.com>
2025-06-10 14:47:33 +0200

    [MOD] Niveau 0 and Music

---
+++ Assets/MusicScenePermanent.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+
+public class MusicScenePermanent : MonoBehaviour
+{
+    public static MusicScenePermanent instance;
+    [SerializeField] private AK.Wwise.Event[] _playMusicEvents;
+    [SerializeField] private AK.Wwise.Event[] _stopMusicEvents;
+    [SerializeField] private float _transitionDelay = 2f;
+    
+    private int _currentMusicIndex = 0;
+    private Coroutine _musicTransitionCoroutine;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        if (_playMusicEvents.Length > 0)
+        {
+            _playMusicEvents[_currentMusicIndex].Post(gameObject);
+        }
+    }
+
+    public void ChangeMusic()
+    {
+        if (_musicTransitionCoroutine != null)
+        {
+            StopCoroutine(_musicTransitionCoroutine);
+        }
+        
+        _musicTransitionCoroutine = StartCoroutine(MusicTransition());
+    }
+
+    private IEnumerator MusicTransition()
+    {
+        if (_stopMusicEvents.Length > 0 && _currentMusicIndex < _stopMusicEvents.Length)
+        {
+            _stopMusicEvents[_currentMusicIndex].Post(gameObject);
+        }
+        
+        yield return new WaitForSeconds(_transitionDelay);
+        
+        _currentMusicIndex = (_currentMusicIndex + 1) % _playMusicEvents.Length;
+        
+        if (_playMusicEvents.Length > 0)
+        {
+            _playMusicEvents[_currentMusicIndex].Post(gameObject);
+        }
+        
+        _musicTransitionCoroutine = null;
+    }
+}
