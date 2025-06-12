8ccec90084ef7e840d8fa577a01b2e8aa1b2606a
Romain Mousty <fun.gaming72200@gmail.com>
2025-06-12 09:28:24 +0200

    [MOD] SecretMenu

--- Assets/Scripts/Managers/SecretMenuManager.cs
+++ Assets/Scripts/Managers/SecretMenuManager.cs
@@ -4,27 +4,19 @@ using UnityEngine;
 
 public class SecretMenuManager : MonoBehaviour
 {
-
     [SerializeField] GameObject MenuSecret;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.L))
         {
-            Debug.Log("Appui sur L");
             ToggleSecretMenu();
         }
     }
 
     void ToggleSecretMenu()
     {
-        Debug.Log("appel de fct");
         if (MenuSecret.active)
         {
             MenuSecret.SetActive(false);
